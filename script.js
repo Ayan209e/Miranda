8debc1a8ae15621bed5acb28aa3637274ad5e791
Ayan209e <ayan209e@gmail.com>
2025-01-17 09:31:55 +0530

    fix: responsive gsap animations added

--- script.js
+++ script.js
@@ -1,23 +1,82 @@
+// LocomotiveScroll
 const scroll = new LocomotiveScroll({
   el: document.querySelector("main"),
   smooth: true,
 });
 
-var tl = gsap.timeline();
+// GSAP MatchMedia for responsive animations
+let mm = gsap.matchMedia();
 
-tl.to("main", {
-  y: "100vh",
-  scale: 0.5,
-  duration: 0,
-});
-tl.to("main", {
-  y: "-30vh",
-  duration: 1,
-});
-tl.to("main", {
-  y: "0vh",
-  rotate: -360,
-  scale: 1,
-  delay: 0.4,
-  duration: 0.7,
-});
+mm.add(
+  {
+    isMobile: "(max-width: 699px)",
+    isTablet: "(min-width: 700px) and (max-width: 1100px)",
+    isDesktop: "(min-width: 1101px)",
+  },
+  (context) => {
+    let { isMobile, isTablet, isDesktop } = context.conditions;
+
+    // GSAP timeline
+    var tl = gsap.timeline();
+
+    // Mobile animations
+    if (isMobile) {
+      tl.to("#animation-trigger", {
+        y: "100vh",
+        scale: 0.6,
+        duration: 0,
+      });
+      tl.to("#animation-trigger", {
+        y: "-80vh",
+        duration: 1,
+      });
+      tl.to("#animation-trigger", {
+        y: "0vh",
+        rotate: -360,
+        scale: 1,
+        delay: 0.3,
+        duration: 0.6,
+      });
+    }
+
+    // Tablet animations
+    if (isTablet) {
+      tl.to("#animation-trigger", {
+        y: "100vh",
+        scale: 0.6,
+        duration: 0,
+      });
+      tl.to("#animation-trigger", {
+        y: "-60vh",
+        duration: 1,
+      });
+      tl.to("#animation-trigger", {
+        y: "0vh",
+        rotate: -360,
+        scale: 1,
+        delay: 0.3,
+        duration: 0.6,
+      });
+    }
+
+    // Desktop animations
+    if (isDesktop) {
+      tl.to("#animation-trigger", {
+        y: "100vh",
+        scale: 0.5,
+        duration: 0,
+      });
+      tl.to("#animation-trigger", {
+        y: "-160vh",
+        duration: 1,
+      });
+      tl.to("#animation-trigger", {
+        y: "0vh",
+        rotate: -360,
+        scale: 1,
+        delay: 0.3,
+        duration: 0.6,
+      });
+    }
+  }
+);
